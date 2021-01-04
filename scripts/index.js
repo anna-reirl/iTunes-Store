db673d77f627ed42c1226dbb5c8f0517dd135cd1
Anna <anna_agabekyan_anna@mail.ru>
2021-01-04 17:45:05 +0400

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,13 @@ const playerBtn = document.querySelectorAll('.player-btn');
 const playerBlock = document.querySelectorAll('.player-block');
 const temp = document.querySelector('.temp')
 
+
+
 const deactivationPlayer = () => {
     temp.style.display = 'none';
     playerBtn.forEach(item => item.classList.remove('active'))
     playerBlock.forEach(item => item.classList.remove('active'))
+
 }
 
 playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -5,6 +5,8 @@ export const radioPlayerInit = () => {
     const radioNavigation = document.querySelector('.radio-navigation')
     const radioItem = document.querySelectorAll('.radio-item')
     const radioStop = document.querySelector('.radio-stop')
+    const radioVolume = document.querySelector('radio-volume')
+    const radioMute = document.querySelector('radio-mute')
 
     const audio = new Audio()
     audio.type = 'audio/aac'
@@ -54,4 +56,20 @@ export const radioPlayerInit = () => {
         }
         changeIconPlay()
     })
-}
+
+//     radioVolume.addEventListener('input', () => {
+//         audio.volume = radioVolume.value / 100
+//         audio.muted = false
+//     })
+
+//     radioMute.addEventListener('click', () => {
+//         audio.muted = !audio.muted
+//     })
+
+//     radioVolume.value = audio.volume * 100
+
+//     return () => {
+//         audio.pause()
+//         changeIconPlay()
+//     }
+ }
